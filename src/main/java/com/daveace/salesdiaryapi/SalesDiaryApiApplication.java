6ea4d17452402dde00ce8c8089aaf7e17098c01a
richard <richardekong290@gmail.com>
2020-03-21 01:04:27 +0100

    create initial project file

---
+++ src/main/java/com/daveace/salesdiaryapi/SalesDiaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.daveace.salesdiaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesDiaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesDiaryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daveace/salesdiaryapi/SalesDiaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daveace.salesdiaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesDiaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
